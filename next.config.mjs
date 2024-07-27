b30bfc5305f51e3cb9d13e7a9d8b5306d578aef6
om salunke <salunkeom474@gmail.com>
2024-07-27 10:43:43 -0700

    error in next.js config file solve

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        hostnamefetching :["localhost","img.clerk.com"]
+    images: {
+        domains: ["img.clerk.com"]
     }
 };
 
